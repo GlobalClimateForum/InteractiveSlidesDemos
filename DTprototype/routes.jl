21c8158de7261e40350db93ec88558bec5a643fd
Jochen Krattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-06-09 22:40:48 +0200

    using @iif macro instead (properly!)

--- DTprototype/routes.jl
+++ DTprototype/routes.jl
@@ -34,7 +34,7 @@ function ui(presentation)
 end
 
 function slide(id, args...; kwargs...)
-    row(class = "q-col-gutter-sm", args..., "v-if='$(id) == current_id'"; kwargs...)
+    row(class = "q-col-gutter-sm", args..., @iif(:($id == current_id)); kwargs...)
 end
 
 route("/") do
