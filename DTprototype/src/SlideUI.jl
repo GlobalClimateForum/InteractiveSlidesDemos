826ab5da3e9df92d9f51adca410820fd46cd282e
Jochen Krattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-07-06 19:27:56 +0200

    added more fields, and also each monitor now has its own drawer

--- DTprototype/src/SlideUI.jl
+++ DTprototype/src/SlideUI.jl
@@ -9,21 +9,52 @@ export get_or_create_pmodel, PresentationModel, reset_counters
 register_mixin(@__MODULE__)
 
 @reactive! mutable struct PresentationModel <: ReactiveModel
-    int1::R{Int} = 0
+    counters::Dict{Symbol, Int8} = Dict(:Bool => 1, :Int => 1, :Vector => 1, :PlotData => 1, :PlotLayout => 1, :PlotConfig => 1)
+    current_id0::R{Int8} = 1
     current_id1::R{Int8} = 1
     current_id2::R{Int8} = 1
     current_id3::R{Int8} = 1
     current_id4::R{Int8} = 1
-    drawer::R{Bool} = false
+    drawer0::R{Bool} = false
+    drawer1::R{Bool} = false
+    drawer2::R{Bool} = false
+    drawer3::R{Bool} = false
+    drawer4::R{Bool} = false
+    int1::R{Int} = 0
+    int2::R{Int} = 0
+    int3::R{Int} = 0
+    int4::R{Int} = 0
+    int5::R{Int} = 0
+    int6::R{Int} = 0
+    int7::R{Int} = 0
+    int8::R{Int} = 0
+    int9::R{Int} = 0
+    int10::R{Int} = 0
     vector1::R{Vector} = []
     vector2::R{Vector} = []
+    vector3::R{Vector} = []
+    vector4::R{Vector} = []
+    vector5::R{Vector} = []
+    vector6::R{Vector} = []
+    vector7::R{Vector} = []
+    vector8::R{Vector} = []
+    vector9::R{Vector} = []
+    vector10::R{Vector} = []
     plotdata1::R{Vector{PlotData}} = [PlotData()]
     plotdata2::R{Vector{PlotData}} = [PlotData()]
+    plotdata3::R{Vector{PlotData}} = [PlotData()]
+    plotdata4::R{Vector{PlotData}} = [PlotData()]
+    plotdata5::R{Vector{PlotData}} = [PlotData()]
     plotlayout1::R{PlotLayout} = PlotLayout()
+    plotlayout2::R{PlotLayout} = PlotLayout()
+    plotlayout3::R{PlotLayout} = PlotLayout()
+    plotlayout4::R{PlotLayout} = PlotLayout()
+    plotlayout5::R{PlotLayout} = PlotLayout()
     plotconfig1::R{PlotConfig} = PlotConfig()
-
-    show_bar::R{Bool} = false
-    counters::Dict{Symbol, Int8} = Dict(:Bool => 1, :Int => 1, :Vector => 1, :PlotData => 1, :PlotLayout => 1, :PlotConfig => 1)
+    plotconfig2::R{PlotConfig} = PlotConfig()
+    plotconfig3::R{PlotConfig} = PlotConfig()
+    plotconfig4::R{PlotConfig} = PlotConfig()
+    plotconfig5::R{PlotConfig} = PlotConfig()
 end
 
 pmodels = PresentationModel[]
@@ -138,10 +169,10 @@ function render_slides(slides_to_render::Vector{Vector}, monitor_id::Int)
     return (titles, bodies)
 end
 
-function standard_menu(slide_titles::Vector{String})
-    drawer(side="left", v__model="drawer", [
+function standard_menu(slide_titles::Vector{String}, m_id::Int)
+    drawer(side="left", v__model="drawer$m_id", [
             list([
-                item(item_section(string(id) * ": " * title), :clickable, @click("current_id = $(id)")) 
+                item(item_section(string(id) * ": " * title), :clickable, @click("current_id$m_id = $(id); drawer$m_id = ! drawer$m_id")) 
                 for 
                 (id, title) in enumerate(slide_titles)
                 ])
@@ -150,7 +181,7 @@ end
 
 function standard_header(num_slides::Int, m_id::Int)
     quasar(:header, quasar(:toolbar, [
-        btn("",icon="menu", @click("drawer = ! drawer"))
+        btn("",icon="menu", @click("drawer$m_id = ! drawer$m_id"))
         btn("",icon="chevron_left", @click("current_id$m_id > 1 ? current_id$m_id-- : null"))
         btn("",icon="navigate_next", @click("current_id$m_id < $num_slides ? current_id$m_id++ : null"))
         ])
@@ -174,7 +205,7 @@ function ui(pmodel::PresentationModel, create_slideshow::Function, request_param
     page(pmodel, prepend = link(href = "$folder/style.css", rel = "stylesheet"),
     [
         StippleUI.Layouts.layout([
-            standard_menu(slide_titles)
+            standard_menu(slide_titles, m_id)
             standard_header(length(slide_titles), m_id)
             standard_footer(m_id, folder)
             StippleUI.Layouts.page_container("",
