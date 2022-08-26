f9ff6349fb95494a224fa7ea24728c174d485278
jochenkrattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2022-08-26 14:44:24 +0200

    uses v-cloak; font of figure bigger

--- src/short_slideshow.jl
+++ src/short_slideshow.jl
@@ -15,7 +15,7 @@ teamsdata = [pd(string("Dummy Team ", t_id)) for t_id in 1:num_teams]
 
 plotdata = @use_field!("VectorPlotData", init_val = deepcopy(teamsdata))
 plotconfig = @use_field!("PlotConfig")
-plotlayout = @use_field!("PlotLayout")
+plotlayout = @use_field!("PlotLayout", init_val = PlotLayout(font = Font("Helvetica, sans-serif", 40, "rgb(31, 31, 31)"),))
 choice = @use_fields!("Vector", init_val = ["Nothing"])
 possible_choices = @use_field!("Vector", init_val = ["Nothing", "Increase", "Decrease", "Sine"])
 
