a79e7f9d7c3f0e8d6345ec34ad90e7684b4a998f
jochenkrattenmacher <10695828+jochenkrattenmacher@users.noreply.github.com>
2023-01-26 12:51:52 +0100

    InteractiveSlides 0.35

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,7 @@
 function main(pmodel::PresentationModel, params::Dict, slides::Vector{Slide})
 num_teams = pmodel.num_teams[]::Int
 
-#Fields and handlers for interactive slides (which come later, see below)
+#Fields for interactive slides (THESE NEED TO BE DEFINED IN THE SAME SCOPE)
 #region
 feedback = @use_field!("String", init_val = "")
 team_ids = collect(1:num_teams)
@@ -29,7 +29,7 @@ plotlayout = @use_field!("PlotLayout", init_val = PlotLayout(
             ticktext = ["Team $t_id" for t_id in team_ids])],
 ))
 
-function handler_helper(t_id)
+function handle_plot(t_id)
     yval = data[event1_choices[t_id].ref[]][Int(event2_choices[t_id].ref[])+1]
     plotdata.ref[1].y[t_id] = yval
     plotdata.ref[1].text[t_id] = "+" * string(round(Int, yval))
@@ -40,18 +40,21 @@ function handler_helper(t_id)
     notify(plotlayout.ref)
 end
 
-#Handlers
+#endregion
+
+#Listeners
+#region
 for t_id in team_ids
     @new_listener(event1_choices[t_id]) do _
-        handler_helper(t_id)
+        handle_plot(t_id)
     end
     @new_listener(event2_choices[t_id]) do _
-        handler_helper(t_id)
+        handle_plot(t_id)
     end
 end
 
-@save(feedback, "feedback")
-@save(choices_table, "choices")
+@save_on(feedback, "feedback")
+@save_on(choices_table, "choices")
 @table_listener(choices_table, 1, event1_choices)
 @table_listener(choices_table, 2, event2_choices, dict = Dict(false => "", true => e2_choice))
 @table_listener(choices_table, 3:5, investment_choices, available_invest_choices)
